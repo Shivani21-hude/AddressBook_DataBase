57c2b4eb5b0f73b3a7ccc0736c9f0f5d3da0a6e3
Shivani21-hude <shivani21hude@gmail.com>
2023-02-16 19:04:56 +0530

    [Shivani]-Create a table with firstname,lastname,address,city,state,zip,phone,and email

--- Database_AddressBook/AddressBookQuery.sql
+++ Database_AddressBook/AddressBookQuery.sql
@@ -2,3 +2,18 @@
 --Uc1
 create database AddressBook
 use AddresssBook
+
+--Uc2
+create table AddressBookTable(
+id int identity(1,1) primary key,
+Firstname varchar(200),
+Lastname varchar(200),
+Address varchar(200),
+City varchar(50),
+State varchar(100),
+Zipcode int,
+Phonenumber int,
+Email varchar(200)
+);
+
+select * from AddressBookTable
